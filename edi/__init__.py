5ef61e407f8bf1cbc7e57af7f702bfa7853f9495
Matthias Luescher <lueschem@gmail.com>
2016-05-03 17:08:55 +0200

    Added basic argument parsing.

--- edi/__init__.py
+++ edi/__init__.py
@@ -20,7 +20,33 @@
 # along with edi.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
+import argparse
+
+class _HelpAction(argparse._HelpAction):
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        parser.print_help()
+        # retrieve subparsers from parser
+        subparsers_actions = [
+            action for action in parser._actions
+            if isinstance(action, argparse._SubParsersAction)]
+        for subparsers_action in subparsers_actions:
+            # get all subparsers and print help
+            for choice, subparser in subparsers_action.choices.items():
+                print(_("* Command '{}':").format(choice))
+                print(subparser.format_help())
+        parser.exit()
 
 def main():
+    parser = argparse.ArgumentParser(description="Setup and manage an embedded development infrastructure.",
+                                     add_help=False)
+    parser.add_argument('--help', action=_HelpAction, help="Show this help")
+    parser.add_argument("-v", "--verbose", action="count", default=0, help="Increase output verbosity (2 levels)")
+
+    parser.add_argument('--version', action="store_true", help="Print version and exit")
+
+    # TODO: set log level
+    parser.parse_args(sys.argv[1:])
+    
     print("Welcome to edi!") 
     print(sys.argv)
