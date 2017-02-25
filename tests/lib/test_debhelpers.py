8c918f7a5367044fe02953b617860e96745270fb
Matthias Lüscher <lueschem@gmail.com>
2017-02-25 23:02:00 +0100

    Automated checksum updates.

--- tests/lib/test_debhelpers.py
+++ tests/lib/test_debhelpers.py
@@ -22,6 +22,9 @@
 import requests
 import requests_mock
 import os
+import hashlib
+import codecs
+import gzip
 from tests.libtesting.fixtures.datadir import datadir
 from edi.lib.debhelpers import download_package
 
@@ -33,8 +36,60 @@ class RepositoryMock():
             '/foodist/dists/stable/Release': 'Release',
             '/foodist/dists/stable/main/binary-amd64/Packages.gz': 'binary-amd64_Packages.gz',
             '/foodist/dists/stable/main/binary-all/Packages.gz': 'binary-all_Packages.gz',
-            '/foodist/pool/main/foo_1.0.0_amd64.deb': 'foo_1.0_amd64.deb'
+            '/foodist/pool/main/foo_1.0_amd64.deb': 'foo_1.0_amd64.deb'
         }
+
+    @staticmethod
+    def _replace_all(data, dict):
+        for key, value in dict.items():
+            data = data.replace(key, value)
+        return data
+
+    def update_checksums(self):
+        deb_names = ['foo_1.0_amd64.deb']
+        checksum_dict = {}
+        for deb_name in deb_names:
+            deb_path = os.path.join(str(self.datadir), deb_name)
+            with open(deb_path, mode='rb') as f:
+                data = f.read()
+                checksum_dict['__{}_md5__'.format(deb_name)] = hashlib.md5(data).hexdigest()
+                checksum_dict['__{}_sha1__'.format(deb_name)] = hashlib.sha1(data).hexdigest()
+                checksum_dict['__{}_sha256__'.format(deb_name)] = hashlib.sha256(data).hexdigest()
+
+        package_names = ['binary-all_Packages', 'binary-amd64_Packages']
+        for package_name in package_names:
+            package_path = os.path.join(str(self.datadir), package_name)
+            with codecs.open(package_path, encoding='utf-8', mode='r') as f:
+                data = f.read()
+                data = self._replace_all(data, checksum_dict)
+            with codecs.open(package_path, mode='w', encoding='utf-8') as f:
+                f.write(data)
+            compressed_package_path =  os.path.join(str(self.datadir), '{}.gz'.format(package_name))
+            with open(package_path, mode='rb') as f:
+                bdata = f.read()
+            with gzip.open(compressed_package_path, mode='wb') as f:
+                f.write(bdata)
+
+        package_names = ['binary-all_Packages', 'binary-amd64_Packages',
+                         'binary-all_Packages.gz', 'binary-amd64_Packages.gz']
+
+        checksum_dict = {}
+        for package_name in package_names:
+            package_path = os.path.join(str(self.datadir), package_name)
+            with open(package_path, mode='rb') as f:
+                data = f.read()
+                checksum_dict['__{}_md5__'.format(package_name)] = hashlib.md5(data).hexdigest()
+                checksum_dict['__{}_sha1__'.format(package_name)] = hashlib.sha1(data).hexdigest()
+                checksum_dict['__{}_sha256__'.format(package_name)] = hashlib.sha256(data).hexdigest()
+
+        release_file = 'Release'
+        release_file_path = os.path.join(str(self.datadir), release_file)
+        with codecs.open(release_file_path, encoding='utf-8', mode='r') as f:
+            data = f.read()
+            data = self._replace_all(data, checksum_dict)
+        with codecs.open(release_file_path, mode='w', encoding='utf-8') as f:
+            f.write(data)
+
 
     def repository_matcher(self, request):
         print('Requesting {}.'.format(request.path_url))
@@ -55,15 +110,22 @@ class RepositoryMock():
 def test_package_download_without_key(datadir):
     with requests_mock.Mocker() as repository_request_mock:
         repository_mock = RepositoryMock(datadir)
+        repository_mock.update_checksums()
         repository_request_mock.add_matcher(repository_mock.repository_matcher)
 
         repository = 'deb http://www.example.com/foodist/ stable main contrib'
         repository_key = None  # 'https://www.example.com/keys/archive-key-8.asc'
         package_name = 'foo'
+        architectures = ['all', 'amd64']
+
         workdir = os.path.join(str(datadir), 'workdir')
         os.mkdir(workdir)
-        architectures = ['all', 'amd64']
+        expected_file = os.path.join(workdir, 'foo_1.0_amd64.deb')
+        assert not os.path.isfile(expected_file)
+
         result = download_package(package_name=package_name, repository=repository,
                                   repository_key=repository_key,
                                   architectures=architectures, workdir=workdir)
-        print('Downloaded {}.'.format(result))
+
+        assert os.path.isfile(expected_file)
+        assert result == expected_file
