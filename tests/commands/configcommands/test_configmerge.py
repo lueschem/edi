b5eaaaa70fa386fd4b77fc61a525558c0198a442
Matthias Lüscher <matthias.luescher@ch.schindler.com>
2017-03-15 13:10:00 +0100

    Added test for new config merge command.

---
+++ tests/commands/configcommands/test_configmerge.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017 Matthias Luescher
+#
+# Authors:
+#  Matthias Luescher
+#
+# This file is part of edi.
+#
+# edi is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# edi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with edi.  If not, see <http://www.gnu.org/licenses/>.
+
+from edi.commands.imagecommands.bootstrap import Bootstrap
+from tests.libtesting.fixtures.configfiles import config_files
+from edi.commands.configcommands.configmerge import Merge
+import edi
+import yaml
+
+
+def test_merge(config_files, capsys):
+    parser = edi._setup_command_line_interface()
+    cli_args = parser.parse_args(['config', 'merge', config_files])
+
+    Merge().run_cli(cli_args)
+    out, err = capsys.readouterr()
+
+    assert err == ''
+    merged_config = yaml.load(out)
+    assert merged_config.get('bootstrap').get('architecture') == 'i386'
