2665dc1aa4d1a498025dc01cea0f3021a7833859
Matthias Lüscher <matthias.luescher@schindler.com>
2017-04-05 01:56:47 -0700

    New version 0.5.1.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def get_install_requires():
 setup(
     name='edi',
 
-    version='0.5.0',
+    version='0.5.1',
 
     description='Embedded Development Infrastructure - edi',
     long_description=long_description,
