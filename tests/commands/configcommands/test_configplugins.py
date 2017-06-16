e23f7faeabb8e76d1db59266215bac70ef51d758
Matthias Lüscher <lueschem@gmail.com>
2017-06-16 23:28:55 +0200

    Added command that dumps the plugins and their dictionaries.

---
+++ tests/commands/configcommands/test_configplugins.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017 Matthias Luescher
+#
+# Authors:
+#  Matthias Luescher
+#
+# This file is part of edi.
+#
+# edi is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# edi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with edi.  If not, see <http://www.gnu.org/licenses/>.
+
+from tests.libtesting.fixtures.configfiles import config_files
+from edi.commands.configcommands.configplugins import Plugins
+import edi
+import yaml
+import os
+
+
+def test_dictionary(config_files, capsys):
+    parser = edi._setup_command_line_interface()
+    cli_args = parser.parse_args(['config', 'plugins', config_files])
+
+    Plugins().run_cli(cli_args)
+    out, err = capsys.readouterr()
+
+    assert err == ''
+    result = yaml.load(out)
+    assert len(result.get('playbooks')) == 3
+    base_system = result.get('playbooks')[0].get('10_base_system')
+    assert 'plugins/playbooks/foo.yml' in base_system.get('path')
+    assert base_system.get('dictionary').get('kernel_package') == 'linux-image-amd64-rt'
+    assert base_system.get('dictionary').get('edi_config_directory') == os.path.dirname(config_files)
