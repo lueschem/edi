8c0cf99c35341ea81f0915640aee96d501d8ef3b
Matthias Lüscher <lueschem@gmail.com>
2018-05-15 16:31:06 +0200

    New version 0.11.7.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def get_install_requires():
 setup(
     name='edi',
 
-    version='0.11.6',
+    version='0.11.7',
 
     description='Embedded Development Infrastructure - edi',
     long_description=long_description,
