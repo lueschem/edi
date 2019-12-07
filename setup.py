7d53fe9c3fa6b6bcf7cfbe3ab442108eb0151484
Matthias Lüscher <lueschem@gmail.com>
2019-12-07 23:36:32 +0100

    New version 1.1.5.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='edi',
 
-    version='1.1.4',
+    version='1.1.5',
 
     description='Embedded Development Infrastructure - edi',
     long_description=long_description,
