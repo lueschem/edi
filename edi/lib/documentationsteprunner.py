0ed92f888a735d7f9b92aa21f5b12c4bb86d512b
Matthias Lüscher <lueschem@gmail.com>
2020-04-09 08:05:40 +0200

    Consider baseline versions when collecting changelog.

--- edi/lib/documentationsteprunner.py
+++ edi/lib/documentationsteprunner.py
@@ -30,6 +30,7 @@ import traceback
 import gzip
 from dateutil import parser
 from debian.changelog import Changelog
+from debian.debian_support import Version
 from edi.lib.helpers import print_success, FatalError
 from edi.lib.helpers import get_workdir
 from edi.lib.configurationparser import remove_passwords
@@ -115,10 +116,12 @@ class DocumentationStepRunner():
         self.config_section = 'documentation_steps'
         self.build_setup = dict()
         self.installed_packages = list()
+        self.baseline_versions = dict()
 
     def fetch_artifact_setup(self):
         self.build_setup = self._get_build_setup()
         self.installed_packages = self._get_installed_packages()
+        self.baseline_versions = self._get_baseline_versions()
 
     def augment_step_parameters(self, parameters):
         augmented_parameters = parameters.copy()
@@ -220,7 +223,7 @@ class DocumentationStepRunner():
 
         if not os.path.isfile(installed_packages_file):
             logging.warning("No file describing the installed packages found in '{}'.".format(installed_packages_file))
-            return dict()
+            return list()
 
         logging.debug("Found file describing the installed packages in '{}'.".format(installed_packages_file))
         with open(installed_packages_file, mode='r') as f:
@@ -229,12 +232,27 @@ class DocumentationStepRunner():
                 raise FatalError("'{}' should contain a list of installed packages.".format(installed_packages_file))
             return installed_packages
 
+    def _get_baseline_versions(self):
+        baseline_file = os.path.join(self.raw_input, 'edi', 'packages-baseline.yml')
+
+        if not os.path.isfile(baseline_file):
+            logging.info("No file describing the packages baseline found in '{}'.".format(baseline_file))
+            return dict()
+
+        logging.debug("Found file describing the packages baseline in '{}'.".format(baseline_file))
+        with open(baseline_file, mode='r') as f:
+            baseline_packages = yaml.safe_load(f.read())
+            if not isinstance(baseline_packages, list):
+                raise FatalError("'{}' should contain a list of baseline packages.".format(baseline_packages))
+            return {self._get_package_value('package', package_dict) : self._get_package_value('version', package_dict)
+                    for package_dict in baseline_packages}
+
     @staticmethod
-    def _get_package_name(package_dict):
-        package_name = package_dict.get('package')
-        if not package_name:
-            raise FatalError("Missing 'package' key in dictionary of installed package.")
-        return package_name
+    def _get_package_value(key, package_dict):
+        value = package_dict.get(key)
+        if not value:
+            raise FatalError("Missing '{}' key in dictionary of package ({}).".format(key, package_dict))
+        return value
 
     @staticmethod
     def _get_output_file(name, node):
@@ -244,13 +262,13 @@ class DocumentationStepRunner():
         return file
 
     def _get_documentation_step_packages(self, parameters):
-        installed_packages = set([self._get_package_name(package) for package in self.installed_packages])
+        installed_packages = set([self._get_package_value('package', package) for package in self.installed_packages])
         include_packages = set(parameters.get('edi_doc_include_packages', installed_packages))
         step_packages = installed_packages.intersection(include_packages)
         exclude_packages = set(parameters.get('edi_doc_exclude_packages', []))
         step_packages = step_packages - exclude_packages
         step_package_list = [package for package in self.installed_packages
-                             if self._get_package_name(package) in step_packages]
+                             if self._get_package_value('package', package) in step_packages]
         return step_package_list
 
     @staticmethod
@@ -274,7 +292,7 @@ class DocumentationStepRunner():
     def _add_changelog(self, package, baseline_date, replacements):
         # TODO: evaluate baseline by package
         package_changelog_path = None
-        package_name = package.get('package')
+        package_name = self._get_package_value('package', package)
         if os.path.isfile(os.path.join(self.raw_input, package_name, 'changelog.Debian.gz')):
             package_changelog_path = os.path.join(self.raw_input, package_name, 'changelog.Debian.gz')
         elif os.path.isfile(os.path.join(self.raw_input, package_name, 'changelog.gz')):
@@ -295,7 +313,10 @@ class DocumentationStepRunner():
                 change_blocks = list()
                 for change_block in changelog:
                     changeblock_date = self._parse_date(change_block.date)
-                    if changeblock_date < baseline_date:
+                    if changeblock_date <= baseline_date:
+                        break
+
+                    if change_block.version <= Version(self.baseline_versions.get(package_name)):
                         break
 
                     block_dict = dict()
@@ -335,12 +356,12 @@ class DocumentationStepRunner():
             baseline_date = self._parse_date(parameters.get('edi_doc_changelog_baseline',
                                                             'Thu, 01 Jan 1970 00:00:00 +0000'))
             # chunk size is currently 1
-            first_package = self._get_package_name(step_packages[0])
-            last_package = self._get_package_name(step_packages[-1])
+            first_package = self._get_package_value('package', step_packages[0])
+            last_package = self._get_package_value('package', step_packages[-1])
 
             for package in step_packages:
                 context = parameters.copy()
-                name = self._get_package_name(package)
+                name = self._get_package_value('package', package)
                 if name == first_package:
                     context['edi_doc_first_chunk'] = True
                 else:
@@ -351,6 +372,7 @@ class DocumentationStepRunner():
                     context['edi_doc_last_chunk'] = False
 
                 package_context = package.copy()
+                package_context['baseline_version'] = self.baseline_versions.get(name, "0.0.0")
 
                 if add_changelog:
                     self._add_changelog(package_context, baseline_date, step_replacements)
