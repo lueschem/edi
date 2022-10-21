7c36185a35f61a0a5c678de45f6fd90d11aabd6f
Matthias Lüscher <lueschem@gmail.com>
2022-10-21 09:20:43 +0200

    New version 1.12.0.

--- edi/lib/versionhelpers.py
+++ edi/lib/versionhelpers.py
@@ -26,7 +26,7 @@ from edi.lib.helpers import FatalError
 
 # The do_release script will update this version!
 # During launchpad debuild neither the git version nor the package version is available.
-edi_fallback_version = '1.11.3'
+edi_fallback_version = '1.12.0'
 
 
 def get_edi_version():
