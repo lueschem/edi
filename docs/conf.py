9e9b89b6f8cdf624f77248649b9b78f41c201db9
Matthias Luescher <lueschem@gmail.com>
2023-10-31 23:41:59 +0100

    Changed header background color.

--- docs/conf.py
+++ docs/conf.py
@@ -120,7 +120,7 @@ html_theme_options = {
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'vcs_pageview_mode': '',
-    'style_nav_header_background': 'lightblue',
+    'style_nav_header_background': 'lightskyblue',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
